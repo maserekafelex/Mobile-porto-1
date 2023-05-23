00bb01fa0fc2dcbd88aa00ecbfa3c188b0436278
AbednegoKibitok <117015422+AbednegoKibitok@users.noreply.github.com>
2023-05-23 18:29:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener
     ("click",() => {
         hamburger.classList.remove("active");
