07202012ae88fa6cb3a71da141bb98685bd4a044
maserekafelex <maserekafelex1@gmail.com>
2023-05-23 19:19:04 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".navMenu");
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener
-    ("click",() => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navMenu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
