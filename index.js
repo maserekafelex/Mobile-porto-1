daa9f4b31b906d2aac7fe5d26f4177ac7afdc1b2
maserekafelex <maserekafelex1@gmail.com>
2023-05-23 16:18:51 +0300

    modified:   .github/workflows/linters.ym,stylelintrc.json, index.html,index.js, package-lock.json and style.cs

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
-
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener
-    ("click",( )=> {
+    ("click",() => {
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }))
