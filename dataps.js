c73f7f3ed4fabcf98ce97938a1f96309dc0cf8d4
maserekafelex <maserekafelex1@gmail.com>
2023-05-28 01:10:44 +0300

    fix required changes

--- dataps.js
+++ dataps.js
@@ -4,23 +4,22 @@ const userMessage = document.getElementById('message');
 const form = document.getElementById('contact-form');
 
 form.addEventListener('input', () => {
-  localStorage.setItem('fullName', userName.value);
-  localStorage.setItem('email', userEmail.value);
-  localStorage.setItem('message', userMessage.value);
+  const contactFormData = {
+    fullName: userName.value,
+    email: userEmail.value,
+    message: userMessage.value,
+  };
+  localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
 });
 
+// Load saved data from localStorage on page load
 window.addEventListener('load', () => {
-  const storedFullName = localStorage.getItem('fullName');
-  const storedEmail = localStorage.getItem('email');
-  const storedMessage = localStorage.getItem('message');
+  const storedContactData = localStorage.getItem('contactFormData');
 
-  if (storedFullName) {
-    userName.value = storedFullName;
-  }
-  if (storedEmail) {
-    userEmail.value = storedEmail;
-  }
-  if (storedMessage) {
-    userMessage.value = storedMessage;
+  if (storedContactData) {
+    const contactData = JSON.parse(storedContactData);
+    userName.value = contactData.fullName;
+    userEmail.value = contactData.email;
+    userMessage.value = contactData.message;
   }
 });
