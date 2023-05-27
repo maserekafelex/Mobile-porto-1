751d9f4e9e656117205353a44e56c0a8e887d2ab
maserekafelex <maserekafelex1@gmail.com>
2023-05-27 23:41:56 +0300

    fix required changes

--- dataps.js
+++ dataps.js
@@ -2,25 +2,25 @@ const userName = document.getElementById('name');
 const userEmail = document.getElementById('email');
 const userMessage = document.getElementById('message');
 const form = document.getElementById('contact-form');
-const contactFormData = {
-  fullName: '',
-  email: '',
-  message: '',
-};
 
 form.addEventListener('input', () => {
-  contactFormData.fullName = userName.value;
-  contactFormData.email = userEmail.value;
-  contactFormData.message = userMessage.value;
-  localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
+  localStorage.setItem('fullName', userName.value);
+  localStorage.setItem('email', userEmail.value);
+  localStorage.setItem('message', userMessage.value);
 });
 
-window.onload = () => {
-  const contactData = JSON.parse(localStorage.getItem('contactFormData'));
+window.addEventListener('load', () => {
+  const storedFullName = localStorage.getItem('fullName');
+  const storedEmail = localStorage.getItem('email');
+  const storedMessage = localStorage.getItem('message');
 
-  if (contactData) {
-    userName.value = contactData.fullName;
-    userEmail.value = contactData.email;
-    userMessage.value = contactData.message;
+  if (storedFullName) {
+    userName.value = storedFullName;
   }
-};
+  if (storedEmail) {
+    userEmail.value = storedEmail;
+  }
+  if (storedMessage) {
+    userMessage.value = storedMessage;
+}
+});
